c0df8d5e3ac2099c3a17885a69c377fb8112d552
StartappzKE <startappzke@gmail.com>
2016-08-15 12:10:17 +0300

    first commit

---
+++ app/src/main/java/buildbrand/startappzke/payapp/LoginActivity.java
@@ -0,0 +1,17 @@
+package buildbrand.startappzke.payapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+}
